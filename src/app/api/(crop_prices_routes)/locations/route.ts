1187d6672570727d0a42b79b2461a45e72b3a2da
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-09-07 17:14:06 +0530

    Added api for fetching list of states and districts from markets for frontend UI

---
+++ src/app/api/(crop_prices_routes)/locations/route.ts
@@ -0,0 +1,21 @@
+import { prisma } from "@/lib/prisma";
+
+export async function GET(): Promise<Response> {
+    const markets = await prisma.market.findMany({
+        distinct: ["state", "district"],
+        select: {
+            state: true,
+            district: true,
+        },
+        orderBy: { state: "asc" },
+    });
+
+    return Response.json(
+        {
+            success: true,
+            message: "Markets (states/districts) fetched",
+            data: markets,
+        },
+        { status: 200 },
+    );
+}
